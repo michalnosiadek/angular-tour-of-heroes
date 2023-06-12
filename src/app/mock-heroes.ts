fe9cb659be324d5c02c581e91d510e7d1a58458d
mi.nosiadek <mi.nosiadek@ecol.com.pl>
2023-06-12 07:24:59 +0200

    Started tour of heroes angular tutorial

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Dr. Nice' },
+  { id: 13, name: 'Mr. Disguise' },
+  { id: 14, name: 'Memory-Man' },
+  { id: 15, name: 'Shield of Honor' },
+  { id: 16, name: 'Neverending Story Teller' },
+  { id: 17, name: 'Handsome Jack' },
+  { id: 18, name: 'Swifty' },
+  { id: 19, name: 'Gorgon' },
+  { id: 20, name: 'Stereotype Buster' },
+];
